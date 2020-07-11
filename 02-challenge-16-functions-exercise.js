38ec6ad3732ad77974d6b636e8f71b0aa3c927df
xaviguasch <xaviguasch@gmail.com>
2020-07-11 21:04:17 +0200

    added challenge: Functions exercise

---
+++ 02-challenge-16-functions-exercise.js
@@ -0,0 +1,9 @@
+//Assignment - Functions Exercise
+
+// Functions are important building blocks in any language. They help ensure that your code follows the DRY principle and that you follow a design principle called Separation of Concerns. Your functions should “do one thing and do it well” (McIlroy). These exercises will help strengthen your function know-how.
+
+// Write a function that accepts two numbers as parameters, and returns the sum.
+
+// Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
+
+// Write a function that accepts one number as a parameter, and returns whether that number is even or odd. (Return the string "even" or "odd");
