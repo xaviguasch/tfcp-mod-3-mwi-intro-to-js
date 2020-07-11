60f9445b26ef72228b10042cf971f4779d699ae3
xaviguasch <xaviguasch@gmail.com>
2020-07-11 13:51:22 +0200

    added Challenge: Loops and Arrays Part 3

---
+++ 01-challenge-13-loops-and-arrays-part-3.js
@@ -0,0 +1,12 @@
+//Assignment - Loops and Arrays Part 3
+
+// Optional Bonus challenge
+// Imagine you have a button that toggles a light on and off. Loop through the following array of numbers and toggle the button the numbers of times for each number. The array [2, 3, 2] would toggle the button 7 times.
+
+// The light is off to start with. Log to the console whether or not the light is on at the end.
+
+// Sample Arrays:
+
+// [2, 5, 435, 4, 3] // "The light is on"
+// [1, 1, 1, 1, 3]   // "The light is on"
+// [9, 3, 4, 2]      // "The light is off"
