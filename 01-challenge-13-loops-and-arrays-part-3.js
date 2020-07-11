2b6953fa593504cfeb1a0041121f8810506fabea
xaviguasch <xaviguasch@gmail.com>
2020-07-11 13:52:25 +0200

    added solution for: Challenge: Loops and Arrays Part 3

--- 01-challenge-13-loops-and-arrays-part-3.js
+++ 01-challenge-13-loops-and-arrays-part-3.js
@@ -10,3 +10,13 @@
 // [2, 5, 435, 4, 3] // "The light is on"
 // [1, 1, 1, 1, 3]   // "The light is on"
 // [9, 3, 4, 2]      // "The light is off"
+
+let arr = [9, 3, 4, 2]
+
+let counter = 0
+
+for (let i = 0; i < arr.length; i++) {
+  counter += arr[i]
+}
+
+counter % 2 === 0 ? console.log('the light is off') : console.log('the light is on')
