8ad711d7fdd6123f6679d53965b67014a117b72e
xaviguasch <xaviguasch@gmail.com>
2020-07-11 21:29:14 +0200

    added challenge: Functions Optional Challenge 2

---
+++ 04-challenge-18-functions-optional-challenge-2.js
@@ -0,0 +1,8 @@
+//Assignment - Functions Optional Challenge 2
+
+// Write a function that accepts a number ‘n’ as a parameter. Then print the first ‘n’ Fibonacci numbers and return their sum.
+// The first numbers are:
+
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+// So if n were 6, the sum of 1+1+2+3+5+8 would be 20
+// Don't hardcode the sequence.
