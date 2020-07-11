d1b37da919726d2a368dcfc7b7f2a9709854477d
xaviguasch <xaviguasch@gmail.com>
2020-07-11 21:23:14 +0200

    added challenge: functions optional challenge 1

---
+++ 03-challenge-17-functions-optional-challenge-1.js
@@ -0,0 +1,5 @@
+//Assignment - Functions Optional Challenge 1
+
+// Write a function that accepts a string as a parameter. If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). If the string is more than twenty characters long, return the first half of the string. You will need to use a string method or two:
+// -slice()
+// -concat() (optional)
