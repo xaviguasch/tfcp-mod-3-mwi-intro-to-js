cac2f1ae498ced7c6b352fad2418ce3db265cbae
xaviguasch <xaviguasch@gmail.com>
2020-07-11 22:07:22 +0200

    added challenge: Functions Optional Challenge 3

---
+++ 05-challenge-19-functions-optional-challenge-3.js
@@ -0,0 +1,3 @@
+//Assignment - Functions Optional Challenge 3
+
+// Write a function that accepts a string as a parameter. Return the most frequently occuring letter in that string. ( White spaces count as a letter )
