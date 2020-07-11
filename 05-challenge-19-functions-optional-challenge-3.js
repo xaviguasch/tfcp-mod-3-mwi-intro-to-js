7afc2a85be87c072466e2fe29fdf9d2c3fdfef1c
xaviguasch <xaviguasch@gmail.com>
2020-07-11 23:03:16 +0200

    added solution for: functions optional challenge 3

--- 05-challenge-19-functions-optional-challenge-3.js
+++ 05-challenge-19-functions-optional-challenge-3.js
@@ -1,3 +1,28 @@
 //Assignment - Functions Optional Challenge 3
 
 // Write a function that accepts a string as a parameter. Return the most frequently occuring letter in that string. ( White spaces count as a letter )
+
+const frequency = function (str) {
+  const charMap = {}
+  let max = 0
+  let maxChar = ''
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
+
+console.log(frequency('madagascar'))
