4141fa757b72de90640cdfb434363fbcdce301a2
xaviguasch <xaviguasch@gmail.com>
2020-07-11 21:54:13 +0200

    added solution for: functions optional challenge 2

--- 04-challenge-18-functions-optional-challenge-2.js
+++ 04-challenge-18-functions-optional-challenge-2.js
@@ -6,3 +6,22 @@
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
 // So if n were 6, the sum of 1+1+2+3+5+8 would be 20
 // Don't hardcode the sequence.
+
+let fibonacci = []
+let finalSum = 0
+
+function numbers(n) {
+  for (let i = 0; i < n; i++) {
+    if (i < 2) {
+      fibonacci.push(1)
+    } else if (i >= 2) {
+      fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1]
+    }
+
+    finalSum += fibonacci[i]
+  }
+
+  return finalSum
+}
+
+console.log(numbers(6))
