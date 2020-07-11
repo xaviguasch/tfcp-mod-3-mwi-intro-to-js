41840a237dd9b07b745b3512a1f823ec40a48383
xaviguasch <xaviguasch@gmail.com>
2020-07-11 21:23:42 +0200

    added solution for: functions optional challenge 1

--- 03-challenge-17-functions-optional-challenge-1.js
+++ 03-challenge-17-functions-optional-challenge-1.js
@@ -3,3 +3,14 @@
 // Write a function that accepts a string as a parameter. If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). If the string is more than twenty characters long, return the first half of the string. You will need to use a string method or two:
 // -slice()
 // -concat() (optional)
+
+function stringer(str) {
+  if (str.length <= 20) {
+    return str.concat(str)
+  } else {
+    return str.slice(0, str.length / 2)
+  }
+}
+
+console.log(stringer('I am the man'))
+console.log(stringer("Meet me in the bathroom that's what she said"))
