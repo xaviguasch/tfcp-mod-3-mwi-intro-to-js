b9993aad38f89572c70da2160cc6c2f217cf939b
xaviguasch <xaviguasch@gmail.com>
2020-07-12 12:24:58 +0200

    added solution for: challenge loop olympics: preliminaries

--- 06-challenge-23-loop-olympics-preliminaries.js
+++ 06-challenge-23-loop-olympics-preliminaries.js
@@ -1,6 +1,19 @@
 //Assignment - Loop Olympics - Preliminaries
 
 // Write a for loop that prints to the console the numbers 0 through 9.
+
+// for (let i = 0; i <= 9; i++) {
+//   console.log(i)
+// }
+
 // Write a for loop that prints to the console 9 through 0.
+// for (let i = 9; i >= 0; i--) {
+//   console.log(i)
+// }
+
 // Write a for loop that prints these fruits to the console.
-// var fruit = ["banana", "orange", "apple", "kiwi"];
+var fruit = ['banana', 'orange', 'apple', 'kiwi']
+
+for (let i = 0; i < fruit.length; i++) {
+  console.log(fruit[i])
+}
