8e0907812eb935ac9cbbf09a612a15937e97da44
xaviguasch <xaviguasch@gmail.com>
2020-07-12 12:55:38 +0200

    added solution for challenge: loop olympics - silver medal

--- 08-challenge-25-loop-olympics-silver-medal.js
+++ 08-challenge-25-loop-olympics-silver-medal.js
@@ -1,9 +1,5 @@
 // Assignment - Loop Olympics - Silver Medal
 
-// Write a loop that will print out all the names of the people of the people array
-// Write a loop that pushes the names into a names array, and the occupations into an occupations array.
-// Write a loop that pushes every other name to an array starting with the first person, in this case "Harrison Ford", and every other occupation to another array starting with, in this case, "Singer".
-
 const peopleArray = [
   {
     name: 'Harrison Ford',
@@ -22,3 +18,38 @@ const peopleArray = [
     occupation: 'Entertainer',
   },
 ]
+
+// Write a loop that will print out all the names of the people of the people array
+
+// for (let i = 0; i < peopleArray.length; i++) {
+//   console.log(peopleArray[i].name)
+// }
+
+// Write a loop that pushes the names into a names array, and the occupations into an occupations array.
+
+// const namesArr = []
+// const occuArr = []
+
+// for (let i = 0; i < peopleArray.length; i++) {
+//   namesArr.push(peopleArray[i].name)
+//   occuArr.push(peopleArray[i].occupation)
+// }
+
+// console.log(namesArr)
+// console.log(occuArr)
+
+// Write a loop that pushes every other name to an array starting with the first person, in this case "Harrison Ford", and every other occupation to another array starting with, in this case, "Singer".
+
+let otherPersonArr = []
+let otherOccuArr = []
+
+for (let i = 0; i < peopleArray.length; i++) {
+  if (i % 2 === 0) {
+    otherPersonArr.push(peopleArray[i].name)
+  } else {
+    otherOccuArr.push(peopleArray[i].occupation)
+  }
+}
+
+console.log(otherPersonArr)
+console.log(otherOccuArr)
