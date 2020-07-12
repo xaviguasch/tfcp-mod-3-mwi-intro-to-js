88e19e9ae518a0b6c9b621d4c7686852c8a497fd
xaviguasch <xaviguasch@gmail.com>
2020-07-12 12:43:16 +0200

    added challenge: loop olympics - silver medal

---
+++ 08-challenge-25-loop-olympics-silver-medal.js
@@ -0,0 +1,24 @@
+// Assignment - Loop Olympics - Silver Medal
+
+// Write a loop that will print out all the names of the people of the people array
+// Write a loop that pushes the names into a names array, and the occupations into an occupations array.
+// Write a loop that pushes every other name to an array starting with the first person, in this case "Harrison Ford", and every other occupation to another array starting with, in this case, "Singer".
+
+const peopleArray = [
+  {
+    name: 'Harrison Ford',
+    occupation: 'Actor',
+  },
+  {
+    name: 'Justin Bieber',
+    occupation: 'Singer',
+  },
+  {
+    name: 'Vladimir Putin',
+    occupation: 'Politician',
+  },
+  {
+    name: 'Oprah',
+    occupation: 'Entertainer',
+  },
+]
