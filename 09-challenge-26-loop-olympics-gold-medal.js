90461c672330cadf7d844780d1827781471a08a9
xaviguasch <xaviguasch@gmail.com>
2020-07-12 13:05:41 +0200

    added challenge: loop olympics - gold medal

---
+++ 09-challenge-26-loop-olympics-gold-medal.js
@@ -0,0 +1,20 @@
+// Assigment - Loop Olympics - Gold Medal
+// 1 - Create an array that mimics a grid like the following using nested for loops:
+// [[0, 0, 0],
+// [0, 0, 0],
+// [0, 0, 0]]
+
+// 2 - Create an array that mimics a grid like the following using nested for loops:
+// [[0, 0, 0],
+// [1, 1, 1],
+// [2, 2, 2]]
+
+//3 - Create an array that mimics a grid like the following using nested for loops:
+// [[0, 1, 2],
+// [0, 1, 2],
+// [0, 1, 2]]
+
+// 4 - Given a grid like the previous ones, write a nested for loop that would change every number to an x.
+// [["x", ...],
+// ["x", ...],
+// ["x",...], ...]
