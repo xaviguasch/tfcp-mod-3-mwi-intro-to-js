01722f9fbdde25bcdc27744e7cf99c8b8ab7bc6b
xaviguasch <xaviguasch@gmail.com>
2020-07-12 13:16:59 +0200

    added solution for challenge loop olympics gold medal

--- 09-challenge-26-loop-olympics-gold-medal.js
+++ 09-challenge-26-loop-olympics-gold-medal.js
@@ -4,17 +4,57 @@
 // [0, 0, 0],
 // [0, 0, 0]]
 
+// const arr = []
+// for (let i = 0; i < 3; i++) {
+//   arr.push([])
+//   for (let j = 0; j < 3; j++) {
+//     arr[i].push(0)
+//   }
+// }
+
+// console.log(arr)
+
 // 2 - Create an array that mimics a grid like the following using nested for loops:
 // [[0, 0, 0],
 // [1, 1, 1],
 // [2, 2, 2]]
+
+// const arr = []
+// for (let i = 0; i < 3; i++) {
+//   arr.push([])
+//   for (let j = 0; j < 3; j++) {
+//     arr[i].push(i)
+//   }
+// }
+
+// console.log(arr)
 
 //3 - Create an array that mimics a grid like the following using nested for loops:
 // [[0, 1, 2],
 // [0, 1, 2],
 // [0, 1, 2]]
 
+// const arr = []
+// for (let i = 0; i < 3; i++) {
+//   arr.push([])
+//   for (let j = 0; j < 3; j++) {
+//     arr[i].push(j)
+//   }
+// }
+
+// console.log(arr)
+
 // 4 - Given a grid like the previous ones, write a nested for loop that would change every number to an x.
 // [["x", ...],
 // ["x", ...],
 // ["x",...], ...]
+
+const arr = []
+for (let i = 0; i < 3; i++) {
+  arr.push([])
+  for (let j = 0; j < 3; j++) {
+    arr[i].push('x')
+  }
+}
+
+console.log(arr)
