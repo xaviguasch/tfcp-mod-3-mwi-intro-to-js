58d7794f0abbe289877dddd170be09ea50fede56
xaviguasch <xaviguasch@gmail.com>
2020-07-12 12:30:33 +0200

    added Challenge: Loop Olympics - Bronze Medal

---
+++ 07-challenge-24-loop-olympics-bronze-medal.js
@@ -0,0 +1,5 @@
+// Assignment - Loop Olympics - Bronze Medal
+// Write a for loop that will push the numbers 0 through 9 to an array.
+// Write a for loop that prints to the console only even numbers 0 through 100.
+// Write a for loop that will push every other fruit to an array.
+// const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
